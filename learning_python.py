3b5527acda23beb56b68084102d4778afb5088ca
ETH1Elohim <ezmax.defi@gmail.com>
2023-03-02 11:23:45 -0500

    basic jupyter python3 csv table

---
+++ learning_python.py
@@ -0,0 +1,10 @@
+# import library_name as alias
+# dataset_name = pd.read_csv('filename.csv')
+# dataset_name.head()
+# dataset_name.dtypes
+
+#import pandas as pd
+#departments = pd.read_csv('departments_example.csv')
+#departments.head()
+#departments.dtypes 
+
